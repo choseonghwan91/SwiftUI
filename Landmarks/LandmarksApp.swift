c97ec760bf815048e80be4df8010d03e53937f50
choseonghwan91 <choseonghwan91@gmail.com>
2023-01-30 21:08:55 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by chosh on 2023/01/30.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
