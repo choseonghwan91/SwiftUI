d9993d35185d01c25662859449b3bc921c93cb3c
choseonghwan91 <choseonghwan91@gmail.com>
2023-02-13 22:29:42 +0900

    add: tutorial 3단계 완료

--- Landmarks/Views/Landmarks/LandmarkRow.swift
+++ Landmarks/Views/Landmarks/LandmarkRow.swift
@@ -2,24 +2,23 @@
 //  LandmarkRow.swift
 //  Landmarks
 //
-//  Created by chosh on 2023/01/31.
+//  Created by chosh on 2023/02/13.
 //
 
 import SwiftUI
 
 struct LandmarkRow: View {
     var landmark: Landmark
-    
+
     var body: some View {
         HStack {
             landmark.image
                 .resizable()
-                .frame(width: 50, height:50)
-            
+                .frame(width: 50, height: 50)
             Text(landmark.name)
-            
+
             Spacer()
-            
+
             if landmark.isFavorite {
                 Image(systemName: "star.fill")
                     .foregroundColor(.yellow)
@@ -29,12 +28,13 @@ struct LandmarkRow: View {
 }
 
 struct LandmarkRow_Previews: PreviewProvider {
+    static var landmarks = ModelData().landmarks
+    
     static var previews: some View {
-        Group{
+        Group {
             LandmarkRow(landmark: landmarks[0])
             LandmarkRow(landmark: landmarks[1])
         }
         .previewLayout(.fixed(width: 300, height: 70))
-        
     }
 }
